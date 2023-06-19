5dc69324473a2693cd2f3e4ddbe2c922ea37d280
梁俊可 <liangjunke198909@outlook.com>
2023-06-19 12:55:53 +0800

    20230619_CAD二次开发测试

--- Day02_CAD绘制基本图形/LJK_AddEntityTool.cs
+++ Day02_CAD绘制基本图形/LJK_AddEntityTool.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,95 @@ using System.Threading.Tasks;
 
 namespace Day02_CAD绘制基本图形
 {
-    class LJK_AddEntityTool
+    public static class LJK_AddEntityTool
     {
+        /// <summary>
+        /// 将图形对象添加到图形文件中
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="entity">图形对象实体</param>
+        /// <returns>图形对象ObjectId</returns>
+        public static ObjectId AddEntityToModelSpace(this Database db, Entity entity)   //此时this关键字使用,成为了db的拓展方法。
+        {
+            //声明ObjectId，用于返回
+            ObjectId entityId = ObjectId.Null;
+            //开启事务处理
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //打开“块表”
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);//父子转行？
+                //打开“块表记录”
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                //添加图形到“块表记录”
+                entityId = btr.AppendEntity(entity);
+                //更新数据信息
+                trans.AddNewlyCreatedDBObject(entity, true);
+                //提交事务
+                trans.Commit();
+            }
+            return entityId;
+        }
 
+        /// <summary>
+        /// 将图形对象添加到图形文件中方法01
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="ents">图形对象实体，可变参数</param>
+        /// <returns>图形对象ObjectId集合</returns>
+        public static List<ObjectId> AddEntityToModelSpace01(this Database db, params Entity[] ents)
+        {
+            List<ObjectId> entityIds;
+            //开启事务处理
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //打开“块表”
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);//父子转行？
+                //打开“块表记录”
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                entityIds = new List<ObjectId>();
+                foreach (var entity in ents)
+                {
+                    // 添加图形到“块表记录”
+                    var entityId = btr.AppendEntity(entity);
+                    entityIds.Append(entityId);
+                    //更新数据信息
+                    trans.AddNewlyCreatedDBObject(entity, true);
+                }
+                //提交事务
+                trans.Commit();
+            }
+            return entityIds;
+        }
+
+        /// <summary>
+        /// 将图形对象添加到图形文件中方法02
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="ents">图形对象实体，可变参数</param>
+        /// <returns>图形对象ObjectId数组</returns>
+        public static ObjectId[] AddEntityToModelSpace02(this Database db, params Entity[] ents)
+        {
+            ObjectId[] entityIds;
+            //开启事务处理
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //打开“块表”
+                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);//父子转行？
+                //打开“块表记录”
+                BlockTableRecord btr = (BlockTableRecord)trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                entityIds = new ObjectId[ents.Length];
+                for (int i = 0; i < ents.Length; i++)
+                {
+                    // 添加图形到“块表记录”
+                    entityIds[i] = btr.AppendEntity(ents[i]);
+                    //更新数据信息
+                    trans.AddNewlyCreatedDBObject(ents[i], true);
+
+                }
+                //提交事务
+                trans.Commit();
+            }
+            return entityIds;
+        }
     }
 }
---
+++ Day02_CAD绘制基本图形/LJK_LineExam_Study.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Day02_CAD绘制基本图形
+{
+
+    public class LJK_LineExam_Study
+    {
+        [CommandMethod("TestDrawOneLine")]
+        public void TestDrawOneLineDemo00()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("--LJK,开始测试！！！---");
+            //------------------------------------------测试一---------------------------------------------
+            Line line01 = new Line(new Point3d(100, 100, 0), new Point3d(200, 100, 0));
+            Database db = HostApplicationServices.WorkingDatabase;
+            //ObjectId entityId = LJK_AddEntityTool.AddEntityToModelSpace(db,line01);//使用静态类的静态方法
+            ObjectId entityId = db.AddEntityToModelSpace(line01);//使用拓展方法
+            editor.WriteMessage("---"+line01.BlockName+"已经绘制完成！！！LJK---");
+        }
+        [CommandMethod("TestDrawMultiSegmentLine01")]
+        public void TestDrawMultiSegmentLineDemo01()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            //------------------------------------------测试二---------------------------------------------
+            Line line02 = new Line(new Point3d(500, 100, 0), new Point3d(600, 100, 0));
+            Line line03 = new Line(new Point3d(500, 200, 0), new Point3d(600, 200, 0));
+            Line line04 = new Line(new Point3d(600, 200, 0), new Point3d(500, 100, 0));
+            List<ObjectId> ids01 = db.AddEntityToModelSpace01(line02, line03, line04);
+            editor.WriteMessage("----使用集合的方式，三条线段完成绘制！！！LJK，测试完成！---");
+        }
+        [CommandMethod("TestDrawMultiSegmentLine02")]
+        public void TestDrawMultiSegmentLineDemo02()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            Line line05 = new Line(new Point3d(1000, 100, 0), new Point3d(1200, 100, 0));
+            Line line06 = new Line(new Point3d(1200, 200, 0), new Point3d(1500, 200, 0));
+            Line line07 = new Line(new Point3d(1500, 200, 0), new Point3d(1000, 100, 0));
+            ObjectId[] ids02 = db.AddEntityToModelSpace02(line05, line06, line07);
+            editor.WriteMessage("----使用数组的方式，三条线段完成绘制！！！LJK，测试完成！---");
+        }
+
+    }
+}
